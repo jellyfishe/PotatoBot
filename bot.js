4d3966a5347ba066b56955175d5f38ab31b53763
Jellyfishe <chelle.wong@mail.utoronto.ca>
2017-06-23 16:49:04 -0400

    Added help and catching some errors

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 
 //------------------Twitter------------------------
-var twitterGuild;
+var twitterGuild = null;
 
 var T = new Twit({
     consumer_key: config.twit_consumer_key,
@@ -32,14 +32,33 @@ stream.on('error', function(err){
 
 stream.on('tweet', function (tweet, err) {
     console.log("Tweet found!");
-    findOnDiscord(twitterGuild, 'channel', 'bottesting').send({embed: createEmbed(tweet.text)});
+    if(twitterGuild)
+        findOnDiscord(twitterGuild, 'channel', 'bottesting').send({embed: createEmbed(tweet.text)}).then(() => console.log("Deliver tweet to Discord")).catch(console.error);
     
 });
-//-----------------Discord Bot --------------------
+//-----------------Discord Bot -------------------- 
 const streamOptions = {seek: 0, volume: 1};
 
 var musicQueue = [];
 var voiceChannel = null;
+
+let responseList = {
+    "help": ":sweet_potato: This bot knows how to: \n\n" +
+    "`1`   **music** `Ex. !music summon`\n" + 
+    "           `summon` - Summons bot to your current voice channel or to the `General` voice channel if there is any. \n" +
+    "           `dismiss` - Removes bot from any voice channel it is currently in. \n" +
+    "           `play` - Plays the first song in the queue. \n" +
+    "           `queue <Youtube URL>` - Queues a song to be played by the bot. \n" +
+    "`2`   **addrole** `@SumMCSS` `<nameOfRole>` - Adds the role(s) to specified user. \n" +
+    "`3`   **cs_meme** - Get a randomized text string in a list about your chances of getting into the CS Post! \n" +
+    "`4`   **Fun** - Type in `ping, tableflip, lenny, shrug, cs_meme` and find out what they do! \n" +
+    "\nPlease prefix these commands with the correct prefix! It is currently set to: `" + config.prefix + "`", 
+    "ping": "pong",
+    "tableflip": "(╯°□°）╯︵ ┻━┻)",
+    "lenny": "( ͡° ͜ʖ ͡°)",
+    "shrug": "¯\_(ツ)_/¯",
+    "cs_meme": cs_meme()
+};
 
 var playRegex = /^(play)\s(.*)$/;
 var youtubeURLRegex = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
@@ -55,18 +74,17 @@ client.on('message', msg => {
     if(!isCommand(msg)){return;}
 
     var command = msg.content.slice(1);
-    if(command === 'help') msg.channel.send('What can I help with?');
-    if(command === 'ping') msg.channel.send('pong');
+    if(responseList[command]) msg.channel.send(responseList[command]);
+    if(command === 'hello') msg.channel.send({embed: createEmbed("testing")});
 
+    var commandArgs = command.split(' ');
     // Music Commands
     if(command.startsWith('music')){
-
-        var mCommand = command.split(' ');
         
-        if(mCommand[1] === 'summon') joinVoiceChannel('General', msg);
-        if(mCommand[1] === 'dismiss') leaveVoiceChannel(msg);
+        if(commandArgs[1] === 'summon') joinVoiceChannel('General', msg);
+        if(commandArgs[1] === 'dismiss') leaveVoiceChannel(msg);
 
-        if(mCommand[1] === 'play'){
+        if(comandArgs[1] === 'play'){
             if(voiceChannel){
                 if(musicQueue.length != 0){
                     msg.channel.send('Playing song');
@@ -80,9 +98,9 @@ client.on('message', msg => {
             }
         }
 
-        if(mCommand[1] === 'queue'){
-            if(mCommand.length > 2 && mCommand[2].match(youtubeURLRegex)){
-                musicQueue.push(mCommand[2]);
+        if(commandArgs[1] === 'queue'){
+            if(commandArgs.length > 2 && commandArgs[2].match(youtubeURLRegex)){
+                musicQueue.push(commandArgs[2]);
                 msg.channel.send('Song added to queue');
             }else {
                 msg.channel.send('Invalid or no Youtube URL specified.');
@@ -90,6 +108,23 @@ client.on('message', msg => {
             
         }
         
+        //Think about making a command that combines queue+play
+        
+    }
+    
+    //General Utility Commands
+    if(command.startsWith('addrole')){
+        let rolesToAdd = [];
+        let person = msg.guild.member(msg.mentions.users.first());
+        
+        //if()
+        
+        for(i = 2; i < commandArgs.length; i++){
+            if(!msg.guild.roles.exists('name', commandArgs[i])) msg.channel.send("The role *" + commandArgs[i] + "* does not exist.");
+            else rolesToAdd.push(msg.guild.roles.find('name', commandArgs[i]));
+        }
+        
+        person.addRoles(rolesToAdd).then(() => console.log("Adding role(s):" + rolesToAdd + " to: " + person.user.username)).catch(console.error);
     }
     
 });
@@ -123,14 +158,14 @@ function joinVoiceChannel(channelName, msg){
     
     voiceChannel = findOnDiscord(msg.guild, 'channel', channelName);
     if(voiceChannel){
-        voiceChannel.join().then(connection => console.log('Connected to ' + voiceChannel.name + ' voice channel!')).catch(console.error);
+        voiceChannel.join().then(connection => console.log('Connecting to ' + voiceChannel.name + ' voice channel...')).catch(console.error);
     }
 }
 
 function leaveVoiceChannel(msg){
     if(voiceChannel){
         voiceChannel.leave();
-        console.log('Disconnected from ' + voiceChannel.name + ' voice channel.');
+        console.log('Disconnecting from ' + voiceChannel.name + ' voice channel...');
         voiceChannel = null;
     }else{
         msg.reply("Nothing to disconnect from.");
@@ -149,28 +184,46 @@ function findOnDiscord(msg, query, search){
     }
 }
 
+function cs_meme(){
+    let memeing = [
+        "Please ask in April/May",
+        "How about you try at UTSG?"
+    ];
+    
+    return memeing[Math.floor(Math.random() * memeing.length)];
+}
+
+// NOTE TO SELF: if want to reference to actual tweet. Do www.twitter.com/<username>/status/<tweet's id_str>
+// If there is picture, access tweet.entities.media.media_url
 function createEmbed(msg){
     const embedOptions = {
-      "title": "MCSS Twitter!",
-      "color": 12492676,
-      "timestamp": new Date(),
-      "footer": {
-        "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-        "text": "Brought to you by the MCSS Team!~"
-      },
-      "thumbnail": {
-        "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-      },
-      "author": {
-        "name": "Potato Bot",
-        "url": "https://discordapp.com",
-        "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-      },
-      "fields": [
-        {
-          "name": "Content",
-          "value": msg
-        }
+        "title": "MCSS Twitter!",
+        "url": "https://twitter.com/utmmcss?lang=en",
+        "description": msg,
+        "color": 16777215,
+        "timestamp": new Date(),
+        "footer": {
+            "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+            "text": "Brought to you by the MCSS Team!~"
+        },
+        "thumbnail": {
+            "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+        },
+        "author": {
+            "name": "Potato Bot",
+            "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+        },
+        "fields": [
+            {
+                "name": "MCSS Website",
+                "value": msg,
+                "inline": true
+            },
+            {
+                "name": "Blah",
+                "value": "hello",
+                "inline": true
+            }
       ]
     };
     return embedOptions;
